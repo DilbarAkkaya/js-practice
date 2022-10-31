523dadf54d16ec1eebb0f290c9f5fe12815081a5
DilbarAkkaya <dilbarmutavalova@gmail.com>
2022-10-31 14:50:55 +0300

    feat: add code switch

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const numberOfFilms = +prompt('How many movies have you watched?', '');
+/* const numberOfFilms = +prompt('How many movies have you watched?', '');
 const personalMoviesDB = {
   count: numberOfFilms,
   movies: {},
@@ -10,6 +10,21 @@ const a = prompt('One of last watched movies?','');
 const b = prompt('How much do you rate it?','');
 const c = prompt('One of last watched movies?','');
 const d = prompt('How much do you rate it?','');
-const movies = {
+personalMoviesDB.movies[a] = b;
+personalMoviesDB.movies[c] = d;
+console.log(personalMoviesDB) */
 
-}
+/* const num = 50;
+switch (num) {
+  case 49: 
+  console.log('It is\'t right');
+  break;
+  case 50:
+    console.log('It is right!');
+    break;
+    case 100:
+      console.log('NOooo');
+      break;
+      default:
+        console.log('Not this time')
+} */
