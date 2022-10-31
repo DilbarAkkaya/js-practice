d21787e25d2e315dd9069e52ba091c58c66307f8
DilbarAkkaya <dilbarmutavalova@gmail.com>
2022-10-31 14:24:08 +0300

    init: init practice lesson, add variables

---
+++ script.js
@@ -0,0 +1,15 @@
+const numberOfFilms = +prompt('How many movies have you watched?', '');
+const personalMoviesDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false
+};
+const a = prompt('One of last watched movies?','');
+const b = prompt('How much do you rate it?','');
+const c = prompt('One of last watched movies?','');
+const d = prompt('How much do you rate it?','');
+const movies = {
+
+}
