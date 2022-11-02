fd93f75ed0d97a621bf24c2aed9a37066942f881
DilbarAkkaya <dilbarmutavalova@gmail.com>
2022-11-02 13:47:30 +0300

    feat: add loop in loop with label

--- script.js
+++ script.js
@@ -182,3 +182,24 @@ for(let i = 1; i < length; i++) {
   result = result + '\n';
 }
 console.log(result) */
+
+/* first: for(let i = 0; i<3; i++) {
+  console.log(`First level: ${i}`);
+  for(j=0; j<3; j++) {
+    console.log(`Second level: ${j}`);
+    for(let k=0; k <3; k++) {
+      if(k=== 2) continue first
+      console.log(`Third level:${k}`)
+    }
+  }
+} */
+first: for(let i = 0; i<3; i++) {
+  console.log(`First level: ${i}`);
+  for(j=0; j<3; j++) {
+    console.log(`Second level: ${j}`);
+    for(let k=0; k <3; k++) {
+      if(k=== 2) break first
+      console.log(`Third level:${k}`)
+    }
+  }
+}
