9e0fb8ef8b71231fa8cdf043ef90f7d011568522
DilbarAkkaya <dilbarmutavalova@gmail.com>
2022-11-02 23:46:35 +0300

    feat: implement task with label

--- script.js
+++ script.js
@@ -271,8 +271,24 @@ for (let i = 0; i < 5; ++i) alert( i ); */
 /* for(let i = 0; i < 3; i++) {
   alert(`number ${i}!`)
 } */
-let i = 0;
+/* let i = 0;
 while(i < 3) {
   alert (`number ${i}!`);
   i++
+} */
+
+/* let i;
+do{
+  i = prompt('Enter a number greater than 100',0);
+}while(i<=100&&i);
+let n = 10; */
+let n=10;
+nextPrime:
+for (let i = 2; i <= n; i++) { // for each i...
+
+  for (let j = 2; j < i; j++) { // look for a divisor..
+    if (i % j == 0) continue nextPrime; // not a prime, go next i
+  }
+
+  alert( i ); // a prime
 }
