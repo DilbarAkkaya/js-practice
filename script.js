e57a6602aaab1bef05b5bf000d09dab4679a8f8e
DilbarAkkaya <dilbarmutavalova@gmail.com>
2022-11-02 23:41:53 +0300

    feat: implement task with for and while

--- script.js
+++ script.js
@@ -262,8 +262,17 @@ while (++i < 5) alert( i ); */
 while (i++ < 5) alert( i ); */
 /* for (let i = 0; i < 5; i++) alert( i );
 for (let i = 0; i < 5; ++i) alert( i ); */
-for (let i = 2; i <= 10; i++) {
+/* for (let i = 2; i <= 10; i++) {
   if (i % 2 == 0) {
     alert( i );
   }
+} */
+
+/* for(let i = 0; i < 3; i++) {
+  alert(`number ${i}!`)
+} */
+let i = 0;
+while(i < 3) {
+  alert (`number ${i}!`);
+  i++
 }
