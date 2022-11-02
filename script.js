51b192df766ff1a1d8e8056b0ad6a0fed6bfd230
DilbarAkkaya <dilbarmutavalova@gmail.com>
2022-11-02 23:38:09 +0300

    feat: implement task with for

--- script.js
+++ script.js
@@ -260,3 +260,10 @@ while (++i < 5) alert( i ); */
 
 /* let i = 0;
 while (i++ < 5) alert( i ); */
+/* for (let i = 0; i < 5; i++) alert( i );
+for (let i = 0; i < 5; ++i) alert( i ); */
+for (let i = 2; i <= 10; i++) {
+  if (i % 2 == 0) {
+    alert( i );
+  }
+}
