cdf22830e046d0b8423f0737755bcb74c2076930
DilbarAkkaya <dilbarmutavalova@gmail.com>
2022-11-04 23:59:28 +0300

    feat: add task hours

--- script.js
+++ script.js
@@ -456,3 +456,31 @@ function findMaxNumber(a,b,c,d) {
       return 0
   } return Math.max(a, b, c, d)
 }
+/*1) Создайте функцию, которая принимает в себя целое число минут и возвращает время в нужном формате строки. (Смотри пример). Обратите внимание на окончание слова "час" - оно меняется в зависимости от цифры. Если вместо аргумента приходит не число, дробное или отрицательное число - функция возвращает строку "Ошибка, проверьте данные"
+Внимание! Давайте пока ограничимся максимум 600ю минутами (10 часов). Так как проверки на большие числа будут раздувать код (33 часа, 31 час, 11 часов и тд). Этого будет достаточно и код будет проверять именно этот промежуток (1 - 10 часов). Но вы можете реализовать и полный скрипт, он тоже должен проходить тесты.*/
+function getTimeFromMinutes(minutesTotal){
+  if(typeof minutesTotal !== 'number' || minutesTotal < 0 || !Number.isInteger(minutesTotal)) {
+return "Ошибкаб проверьте данные"
+  }
+const hours = Math.floor(minutesTotal /60);
+const minutes = minutesTotal % 60;
+let hoursStr = '';
+
+switch (hours) {
+    case 0: 
+        hoursStr = 'часов';
+        break;
+    case 1:
+        hoursStr = 'час';
+        break;
+    case 2:
+    case 3:
+    case 4:
+        hoursStr = 'часа';
+        break;
+    default:
+        hoursStr = 'часов';
+}
+
+return `Это ${hours} ${hoursStr} и ${minutes} минут`;
+}
