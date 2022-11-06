e8bcf75eeaf58181e16bc34849895a2fd11ae4d0
DilbarAkkaya <dilbarmutavalova@gmail.com>
2022-11-06 16:37:29 +0300

    feat: add task for obj les

--- script.js
+++ script.js
@@ -603,7 +603,7 @@ console.log(obj); */
 /* 1) Напишите функцию showExperience, которая будет принимать в себя объект со всеми данными и возвращать строку с опытом.
 Пример: showExperience(personalPlanPeter) => '1 month'
 P.S. желательно использовать деструктуризацию, но не обязательно */
-const personalPlanPeter = {
+/* const personalPlanPeter = {
   name: "Peter",
   age: "29",
   skills: {
@@ -618,4 +618,28 @@ const personalPlanPeter = {
 function showExperience(plan){
   const {exp}=plan.skills;
   return exp;
+} */
+/*2) Напишите функцию showProgrammingLangs, которая будет принимать в себя объект со всеми данными и возвращать строку в нужном виде.
+Пример: showProgrammingLangs(personalPlanPeter)  =>
+"Язык js изучен на 20% Язык php изучен на 10%"
+Причем функция должна работать вне зависимости от количества языков. Если ни один не указан, то возвращается пустая строка.
+P.S. Для переноса строки используется \n в конце строки.*/
+const personalPlanPeter = {
+  name: "Peter",
+  age: "29",
+  skills: {
+    languages: ['ru', 'eng'],
+    programmingLangs: {
+      js:'20%',
+      php:'10%'
+    },
+    exp: '1 month',
+}}
+function showProgrammingLangs(plan){
+const {programmingLangs} = plan.skills;
+let str='';
+for(let key in programmingLangs) {
+  str+=`Язык ${key} изучен на ${programmingLangs[key]}\n`
+}
+return '';
 }
