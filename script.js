f04109bc49971f5f7759e061299a2ad4548a897f
DilbarAkkaya <dilbarmutavalova@gmail.com>
2022-12-23 13:37:29 +0300

    feat: add binarysearch function

--- script.js
+++ script.js
@@ -78,5 +78,28 @@ function searchLinear (t, arr) {
   }
   return null
 }
-console.log(searchLinear(5, [1, 3, 6, 5, 8]))
+/* console.log(searchLinear(5, [1, 3, 6, 5, 8]))
 console.log(count)
+ */
+function binarySearch(arr, t){
+  let start = 0;
+  let end = arr.length;
+  let middle;
+  let found = false;
+  let position = -1;
+ while (found === false && start<=end) {
+  middle = Math.floor((start+end)/2);
+  if(arr[middle] === t) {
+    found = true;
+    position = middle;
+    return position;
+  }
+  if(t<arr[middle]){
+    end=middle-1
+  } else {
+    start = middle+1
+  }
+ }
+ return position;
+}
+console.log(binarySearch([5, 6, 7], 6))
