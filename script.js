16d76f19998630924668cecfb1beb3dc353cf162
DilbarAkkaya <dilbarmutavalova@gmail.com>
2022-12-23 16:31:54 +0300

    feat: add quicksort func

--- script.js
+++ script.js
@@ -150,3 +150,28 @@ if(n === 1) {
 return n * factorial(n-1)
 }
 console.log(factorial(10))
+
+function quickSort(arr){
+  if(arr.length<=1){
+    return arr;
+  }
+  let pivotIndex = Math.floor(arr.length/2);
+  let pivot = arr[pivotIndex];
+  let less= []
+  let greater = []
+  for (let i = 0; i <arr.length; i++) {
+    count=count+1
+    if (i===pivotIndex) {
+      continue
+    }
+    if(arr[i]<pivot){
+      less.push(arr[i])
+    } else {
+      greater.push(arr[i])
+    }
+  }
+  return [...quickSort(less), pivot, ...quickSort(greater)]
+}
+
+console.log(quickSort([1, 5, 0, 8, 10, -2, 5, 9]))
+console.log(count)
