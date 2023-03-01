c2ae6b86b0c753f0ec8e54b7407e85df8b4c4073
DilbarAkkaya <dilbarmutavalova@gmail.com>
2023-03-02 00:58:07 +0300

    feat: add symbol.iterator

--- script.js
+++ script.js
@@ -408,3 +408,30 @@ function Base(str){
     delete clone.name;
     let nameDescr = Object.getOwnPropertyDescriptor(clone, 'name')
   console.log(nameDescr);
+
+  let range = {
+    from: 1,
+    to:5,
+  };
+
+  range[Symbol.iterator] = function(){
+    return {
+      current: this.from,
+      last: this.to,
+      next() {
+        if(this.current <= this.last) {
+          return {
+            done: false,
+            value: this.current, 
+          }
+        } else {
+          return {
+            done: true,
+          }
+        }
+      }
+    }
+  }
+  for (let num of range) {
+  
+  }
