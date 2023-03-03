138829dd3c956c9e1b44d6f6ce44a96b07e13084
DilbarAkkaya <dilbarmutavalova@gmail.com>
2023-03-03 13:44:26 +0300

    feat: sort with loop

--- script.js
+++ script.js
@@ -516,3 +516,17 @@ function flattenConcat(arr){
 console.log(flattenArr([[1, 2], [3, 4, 5], 6]))
 
 console.log(flattenConcat([[1, 2], [3, 4, 5], 6]))
+
+function sortArr(arr){
+  for(let i = 0; i<arr.length;i++){
+    for (let j=i+1; j<arr.length;j++){
+      if (arr[i] < arr[j]) {
+        let temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+    return arr
+}
+console.log(sortArr([1, 3, 2, 7, 4]))
